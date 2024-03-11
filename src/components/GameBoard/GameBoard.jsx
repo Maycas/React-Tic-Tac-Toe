40542d6f0d9cc7fea6921f9ebff23f40263c1f11
Marc Maycas <marc.maycas@gmail.com>
2024-03-11 19:58:52 +0100

    Updating states in immutable way

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,42 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    /* Not recommended approach. It's better to update
+     Object-State Immutably (objects, arrays) as you would 
+     update the value you change immediately without waiting 
+     for the scheduler to operate and will have side effects 
+     or strange bugs */
+    // setGameBoard((prevGameBoard) => {
+    //   prevGameBoard[rowIndex, colIndex] = 'X'
+    //   return prevGameBoard
+    // })
+    /* Recommended approach: Use the spread operator to create a shallow copy */
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id='game-board'>
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() =>
+                    handleSelectSquare(rowIndex, colIndex)
+                  }
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
