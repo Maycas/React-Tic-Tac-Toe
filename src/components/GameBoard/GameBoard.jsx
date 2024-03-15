d494e3b7453bca5ff3ae692831b42355b196f866
Marc Maycas <marc.maycas@gmail.com>
2024-03-15 14:20:04 +0100

    Derive if the game has a winner without additional states and calculating the winner

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,46 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   /* Not recommended approach. It's better to update
-  //    Object-State Immutably (objects, arrays) as you would
-  //    update the value you change immediately without waiting
-  //    for the scheduler to operate and will have side effects
-  //    or strange bugs */
-  //   // setGameBoard((prevGameBoard) => {
-  //   //   prevGameBoard[rowIndex, colIndex] = 'X'
-  //   //   return prevGameBoard
-  //   // })
-  //   /* Recommended approach: Use the spread operator to create a shallow copy */
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare()
-  // }
-
-  // Right approach is to manage as little state as needed and derive it from the state in a higher component
-  let gameBoard = initialGameBoard;
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id='game-board'>
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
