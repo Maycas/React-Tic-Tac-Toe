65482ae1d1c62fd6aacdaf1827de2fd1075458cf
Marc Maycas <marc.maycas@gmail.com>
2024-03-15 14:36:11 +0100

    Show winner player name

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol, isActive }) {
+export default function Player({ name, symbol, isActive, onChangeName }) {
   const [playerName, setPlayerName] = useState(name);
   const [isEditMode, toggleEditMode] = useState(false);
 
   function handleEditClick() {
     toggleEditMode((editing) => !editing); // toggleEditMode(!isEditMode) is not advised. Recommendation is to make sure we don't find unexpected rendering effects when updating a state based on a previous state value, because of React scheduling of React states
+    if (isEditMode) onChangeName(symbol, playerName);
   }
 
   function handleChange(event) {
